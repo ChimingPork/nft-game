67f7568a529c1130799e58f6dd9bc2f8bda7be53
Josh Bellingham <92236549+ChimingPork@users.noreply.github.com>
2022-10-25 22:32:30 -0700

    Placeholder to turn Toast into component

---
+++ frontend/src/Components/Toast/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Toast.css';
+
+const Toast = () => {
+  return (
+    <div id="toast"><div id="img">Icon</div><div id="desc">A notification message..</div></div>
+  );
+};
+
+export default Toast;
